03de9db64efcbd57c2a55d35c01cbf9871b8d301
Nettle <ctmann@gmail.com>
2018-09-15 19:13:14 -0400

    basic function...work-around for my inability to scrape headings

--- Code/1_Scrape.R
+++ Code/1_Scrape.R
@@ -2,65 +2,70 @@
 # Libraries ---------------------------------------------------------------
 library(tidyverse)
 library(rvest)
-
-# Create Refs
-
-# Navy
-# Example with one year
-library(tidyverse)
-library(rvest)
-
-my.url <- read_html("http://www.secnav.navy.mil/fmc/fmb/Pages/Fiscal-Year-2019.aspx") %>% 
-  html_nodes("#sharePointMainContent") 
-
-hyperlink.title <- my.url %>% 
-  html_nodes("li") %>% 
-  html_text()
-
-hyperlink <- my.url %>% 
-  html_nodes("li") %>% 
-  html_nodes("a") %>% 
-  html_attr("href")
-
-df <- tibble(title, hyperlink.title)
-df
+library(stringr)
 
 
-View(df)
+navy_function <- function(my.site){
+  x <- read_html(my.site) %>% 
+  html_nodes(".s4-wpcell-plain") %>% 
+  map_df(~{
+    titles <- .x %>% html_nodes('li') %>% html_text()
+    links <- .x %>% html_nodes('a') %>% html_attr("href")
+    data_frame( titles, links)
+  })
+  return(x) }
 
-library(tidyverse)
-library(rvest)
+#Create a df
 
-my.url <- read_html("http://www.secnav.navy.mil/fmc/fmb/Pages/Fiscal-Year-2019.aspx") %>% 
-  html_nodes("#sharePointMainContent") 
+navy <- tibble(FY = as.character(2017),
+              hyperlink = str_c("http://www.secnav.navy.mil/fmc/fmb/Pages/Fiscal-Year-", 2017, ".aspx"),
+              Department = "Navy")
+
+navy <- navy %>%
+  mutate(data = map(hyperlink, navy_function)) %>% unnest() %>%
+  mutate(
+    sorting.id = row_number(),
+    service = case_when(
+      str_detect(titles, "Navy") ~ "Navy",
+      str_detect(titles, "Marine") ~ "Marine.Corps"
+    ),
+    program = case_when(
+      str_detect(titles, "Personnel") ~ "Military.Personnel",
+      str_detect(titles, "Operation") ~ "O.M",
+      str_detect(titles, "Capital") ~ "NWCF",
+      str_detect(titles,
+        "Weapons|Shipbuilding|Sealift|Procurement Marine Corps|Consolidated"
+      ) ~ "Procurement.General",
+      str_detect(titles, "Aircraft") ~ "Procurement.Aircraft",
+      str_detect(titles, "Other Procurement") ~ "Procurement.Other",
+      str_detect(titles, "Ammunition") ~ "Procurement.Ammunition",
+      str_detect(titles, "Realignment") ~ "BRAC",
+      str_detect(titles, "Family") ~ "Family.Housing",
+      str_detect(titles, "Construction") ~ "Military.Construction",
+      str_detect(titles, "Research") ~ "RDT.E",
+      str_detect(titles, "Contingency") ~ "OCO",
+      TRUE ~ "summary.docs")) %>% 
+  group_by(program) %>% 
+  mutate(amended.or.not = if_else(
+    any(str_detect(titles, "Amend")), "PBR.for.this.program.was.amended", "not.amended")) %>% 
+  ungroup()
 
 
-subject.heading <- my.url %>% 
-  html_nodes("h3") %>% 
-  html_text() %>% str_trim()
-
-# Notepad-------------------------
-# my.url %>% 
-#   html_node("#sharePointMainContent") %>% 
-#   html_nodes("table") %>% 
-#   html_table(fill = T, header = T)
-
-# ms-standardheader ms-WPTitle
+  
+  
+  
+  
+  
+  
 
 
-# https://github.com/hadley/rvest/issues/12
-# nodes <- "http://pyvideo.org/category/50/pycon-us-2014" %>%
-#   html %>%
-#   html_nodes("div.video-summary-data")
-# 
-# column <- function(x) nodes %>% html_node(xpath = x) %>% html_text()
-# 
-# df <- data.frame(
-#   title = column("div[1]//a"),
-#   author = column("div[3]//a"),
-#   date = column("div[4]//small[1]"),
-#   language = column("div[4]//small[2]"),
-#   description = column("div[5]//p"),
-#   stringsAsFactors = FALSE
-# )
+
+
+
+
+
+
+
+
+
 
